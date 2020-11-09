cbf4ce5e0d3adadccedf9773f9a3bb1a9445d9ee
Julrum <julrumg@gmail.com>
2020-11-09 11:28:51 +0900

    enhancement: Add datasheet feature

---
+++ src/DataSheet.style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const table = styled.div`
+  border-spacing: 0;
+  border: 1px solid black;
+`;
--- src/DataSheet.tsx
+++ src/DataSheet.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import produce from 'immer';
+import { useTable, useFlexLayout } from 'react-table';
 
 import { DataSheetProps } from './DataSheet.type';
+import * as styled from './DataSheet.style';
 
 const DataSheet: React.FC<DataSheetProps> = ({}) => {
   const [count, setCount] = useState(0);
@@ -41,7 +43,70 @@ const DataSheet: React.FC<DataSheetProps> = ({}) => {
     setIsMouseDown(true);
   };
 
-  return <div>excel</div>;
+  const Table = ({ columns, data }: any) => {
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+    } = useTable(
+      {
+        columns,
+        data,
+      },
+      useFlexLayout,
+    );
+
+    return (
+      <>
+        <styled.table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup, i) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.slice(0, 10).map((row, i: number) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell, i: number) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </styled.table>
+      </>
+    );
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Column 1',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Column 2',
+        accessor: 'col2',
+      },
+    ],
+    [],
+  );
+
+  const data: any[] = [];
+  return <Table columns={columns} data={data} />;
 };
 
 export default DataSheet;
